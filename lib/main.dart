39e138a064903b2b5de49ef3265cbd5f09843e1a
Techno Prashant <prashant09mca@gmail.com>
2020-08-18 18:49:07 +0530

    update methods

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:share_apps/share_apps.dart';
 
 void main() {
-  ShareApps.init(AppId: 'XXXXXXXXXX', secrateKey: 'XXXXXXXXXXXX');
+  ShareApps.init(appId: 'XXXXXXXXXX', secreteKey: 'XXXXXXXXXXXX');
   runApp(MyApp());
 }
 
@@ -28,11 +28,30 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('Share App Demo'),
       ),
       body: Center(
-        child: RaisedButton(
-          child: Text('Invite Friends'),
-          onPressed: () {
-            ShareApps.sendInvitation(context: context);
-          },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RaisedButton(
+              child: Text('Invite Friends'),
+              onPressed: () {
+                ShareApps.sendInvitation(context: context);
+              },
+            ),
+            RaisedButton(
+              child: Text('Invite Friends Background'),
+              onPressed: () {
+                ShareApps.sendInvitationInBackground(
+                    context: context,
+                    firstName: 'ABC',
+                    lastName: 'XYZ',
+                    countryName: 'United States',
+                    email: 'xyz@gmail.com',
+                    language: 'ENGLISH',
+                    number: '444123456');
+              },
+            ),
+          ],
         ),
       ),
     );
